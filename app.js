5d34714e07bc6be76dd03e3221a949d61144bd2b
Benson.YBS <yehbo0921@gmail.com>
2022-09-30 03:05:05 +0800

    S2U29: Set routes in Express

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const PORT = 3000
 app.get('/', (req, res) => {
   res.send(`This is Express Web App`)
 })
+app.get('/food', (req, res) => {
+  res.send('My favorite food is ice cream')
+})
 
+// localhost:3000/popular/languages  ==> JavaScript is a popular language
+app.get('/popular/languages', (req, res) => {
+  res.send('JavaScript is a popular language')
+})
 // Listen the server when it started
 app.listen(PORT, () => {
   console.log(`Express is running on http://localhost:${PORT}`)

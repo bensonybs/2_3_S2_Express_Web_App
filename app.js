9c4f641da895f848f1dff5229a66a5bd36d917b0
Benson.YBS <yehbo0921@gmail.com>
2022-09-30 03:32:07 +0800

    S2U36: Import Bootstrap

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // routes setting
+app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.render('index')
 })

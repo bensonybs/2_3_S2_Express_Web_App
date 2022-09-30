c164570e27f278a69bd59323fad9354be6bb4bdc
Benson.YBS <yehbo0921@gmail.com>
2022-09-30 21:09:47 +0800

    S2U38: Using Each Loop in View

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.get('/', (req, res) => {
     title: 'Jurassic World 3: Fallen Kingdom',
     image: 'https://movie-list.alphacamp.io/posters/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg'
   }
-  res.render('index', { movie: movieOne})
+  const numberList = [1, 2, 3, 4, 5, 6, 7, 8]
+  res.render('index', { movie: movieOne, numberList: numberList})
 })
 
 // start and listen on the Express server
